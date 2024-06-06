37b4447560fdb4b7ffd82bd48c5e8f9f4bcc7baa
Harsh3991 <dhobleharshwardhan@gmail.com>
2024-06-06 22:01:20 +0530

    created post endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -6,12 +6,27 @@ const app = express();
 const port = process.env.PORT || 3000;
 const booksModel = require("./model/books.model");
 
+app.use(express.json());
+
 connectDB();
 
+//Get book images endpoint
 app.get("/Books/Get", async (req, res) => {
   try {
     let result = await booksModel.find();
     res.send({ msg: "Fetched the data successfully", data: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+//Post book images endpoint
+app.post("/Books/Post", async (req, res) => {
+  try {
+    let book = new booksModel(req.body);
+    let result = await book.save();
+    res.send({ msg: "Data saved successfully", data: result });
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
